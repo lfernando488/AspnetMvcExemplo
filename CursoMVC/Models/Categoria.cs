fc8715c727d9c434ad2ee6ba61ad886b271f41fb
lfernando488 <lfernando488@gmail.com>
2021-02-17 19:23:11 -0300

    Validação e conexao com o SqlServerModel Produto

--- CursoMVC/Models/Categoria.cs
+++ CursoMVC/Models/Categoria.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CursoMVC.Models{
     public class Categoria{
+        
         public int id { get; set; }
+
+        [Display(Name= "Descrição")]
+        [Required(ErrorMessage = " O campo descrição é obrigatório")]
         public string descricao { get; set; }
         public List<Produto> produtos { get; set; }
     }
--- CursoMVC/Models/Produto.cs
+++ CursoMVC/Models/Produto.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CursoMVC.Models{
     public class Produto{
         public int id { get; set; }
+        
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = " O campo descrição é obrigatório")]
         public string descricao { get; set; }
+
+        [Display(Name = "Quantidade")]
+        [Range( 1, 500, ErrorMessage = "Valor máximo excedido")]
         public int quantidade { get; set; }
+
         public int categoriaId { get; set; }
+        
         public Categoria categoria { get; set; }
 
 
